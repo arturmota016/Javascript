eb0db5e0fab36b788ddb57fe000a5bd48fae29b9
arturmota <arturmota016@gmail.com>
2025-08-27 09:25:22 -0300

    com calculadora

--- nodejs/modulos/app.js
+++ nodejs/modulos/app.js
@@ -1,4 +1,8 @@
-const saudacao = require('./meuModulo'); // Importando o módulo
+const modulo = require('./meuModulo'); // Importando o módulo
+const calculadora_idade = require('./calculadora-idade');
 
-const mensagem = saudacao('Joédio'); // Executando a função
+const mensagem = modulo.saudacao('artur'); // Executando a função
+const idade2025 = calculadora_idade.idade(2009,'artur');
+
 console.log(mensagem);
+console.log(idade2025);
